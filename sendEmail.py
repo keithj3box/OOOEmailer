fe41d50c45e471d792d385bf1c731cce70d54875
Keith <kjohnson@box.com>
2018-11-15 09:10:58 -0800

    add link to PTO policy

--- sendEmail.py
+++ sendEmail.py
@@ -16,7 +16,7 @@ from creds import gmailScopes, gmailTokenLocation, gmailCredsLocation, senderEma
 # If modifying these scopes, delete the file token.json.
 SCOPES = gmailScopes
 
-agentEmailBody = 'Hey there! \n\n[[This is an automated message.]] \n\n It looks like you may be on PTO tomorrow based on an event on the User Services calendar. \n\n Just a reminder to remember to mark yourself \'OOO\' in Zendesk before you leave. Otherwise, you will be assigned cases. \n\n Here\'s a link to the new PTO policy: LINK \n\n [[By the way, these emails may not always come, so please don\'t start to rely on them!]]'
+agentEmailBody = 'Hey there! \n\n[[This is an automated message.]] \n\n It looks like you may be on PTO tomorrow based on an event on the User Services calendar. \n\n Just a reminder to remember to mark yourself \'OOO\' in Zendesk before you leave. Otherwise, you will be assigned cases. \n\n Here\'s a link to the new PTO policy: https://confluence.inside-box.net/display/CONTR/User+Services+PTO+Policy \n\n [[By the way, these emails may not always come, so please don\'t start to rely on them!]]'
 agentEmailSubject = 'AUTOMATED MESSAGE: Upcoming PTO Reminder'
 
 managerEmailSubject = 'AUTOMATED MESSAGE: Possible PTO agent(s) not OOO in Zendesk.'
